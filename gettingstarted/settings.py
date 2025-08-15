19998519200ee257db1da3d8e5d34d440e077c8e
Carlo-Jacques <65261011+Carlo-Jacques@users.noreply.github.com>
2025-08-15 03:14:07 +0000

    email script in html

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'charlotteswebdesignllc@gmail.com'
 EMAIL_HOST_PASSWORD = 'xnbk nure updh ciym'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # Before using your Heroku app in production, make sure to review Django's deployment checklist:
